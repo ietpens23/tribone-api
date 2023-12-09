91af7d4a62b542894c3079a3b28888e4190cef5a
muqsith <01muqsith@gmail.com>
2023-12-09 19:23:28 +0700

    monggo di coba keamanannya :)

--- updateProfileImage.php
+++ updateProfileImage.php
@@ -4,7 +4,8 @@ if ($method == "OPTIONS") {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Headers: access');
     header('Access-Control-Allow-Methods: PUT');
-    header('Content-Type: application/json');
+    header('Content-Type: application/json; charset=UTF-8');
+    header('X-Content-Type-Options: nosniff');
     header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
     header('HTTP/1.1 200 OK');
     die();
@@ -13,12 +14,19 @@ if ($method == "OPTIONS") {
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: access');
 header('Access-Control-Allow-Methods: PUT');
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=UTF-8');
+header('X-Content-Type-Options: nosniff');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 
 require_once __DIR__ . '/src/dbConnect.php';
 require_once __DIR__ . '/src/jwtHandler.php';
 require_once __DIR__ . '/src/jsonHandler.php';
+
+function isImageFile($filename) {
+    $allowed_extensions = array('jpg', 'jpeg', 'png');
+    $file_extension = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
+    return in_array($file_extension, $allowed_extensions);
+}
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') :
     $headers = getallheaders();
@@ -26,10 +34,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') :
         $data = decodeToken($matches[1]);
         $userId = (int) $data;
         if (!is_numeric($data)) jsonHandler(401, 'User tidak valid!');
-
+        
         $photo_tmp = $_FILES['photo']['tmp_name'];
         $photo_name = $_FILES['photo']['name'];
-
+        if (isImageFile($photo_name)){
         move_uploaded_file($photo_tmp, 'profile_img/' . $photo_name);
 
         // if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
@@ -44,12 +52,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') :
         //     $photo_name . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/profile_img/' . $photo_name;
         // }
 
-        $photodir = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], "/")) . '/profile_img/' . $photo_name;
+        $photodir = $_SERVER['REQUEST_SCHEME'] . 'https://' . $_SERVER['HTTP_HOST'] . substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], "/")) . '/profile_img/' . $photo_name;
         $sql = "UPDATE `users` SET `profile` = '$photodir' WHERE `id` = '$userId'";
         $query = mysqli_query($conn, $sql);
         if ($query) jsonHandler(200, $photodir);
+        } else {
         jsonHandler(500, 'Terjadi kesalahan.');
-
+        };
     endif;
     jsonHandler(403, "Authorization Token salah!");
 
